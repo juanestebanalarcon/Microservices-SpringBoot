5d4999fea440c155debaebaafe9ad0f015b322c7
juanestebanalarcon <jalarcon_logyca@outlook.com>
2022-09-06 00:02:27 -0500

    Zuul service server

--- servicio-zuul-server/service-zuul-server/src/main/java/com/jeam/springboot/app/zuul/servicezuulserver/ServiceZuulServerApplication.java
+++ servicio-zuul-server/service-zuul-server/src/main/java/com/jeam/springboot/app/zuul/servicezuulserver/ServiceZuulServerApplication.java
@@ -2,7 +2,11 @@ package com.jeam.springboot.app.zuul.servicezuulserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class ServiceZuulServerApplication {
 
---
+++ servicio-zuul-server/service-zuul-server/src/main/java/com/jeam/springboot/app/zuul/servicezuulserver/filters/PosTiempoTranscurridoFilter.java
@@ -0,0 +1,42 @@
+package com.jeam.springboot.app.zuul.servicezuulserver.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.logging.Logger;
+
+@Component
+public class PosTiempoTranscurridoFilter extends ZuulFilter {
+
+    private static final Logger Log = (Logger) LoggerFactory.getLogger(PosTiempoTranscurridoFilter.class);
+
+
+    @Override
+    public String filterType() {
+        return "post";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request=requestContext.getRequest();
+        Log.info(String.format("%s request enrutado app %s",request.getMethod(),request.getRequestURL().toString()));
+        Long startTime = System.currentTimeMillis();
+        request.setAttribute("startTime", startTime);
+        return null;
+    }
+}
